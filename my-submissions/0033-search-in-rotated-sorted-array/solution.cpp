3957639d018033bfc5c3fa9d63e8756b08f32308
alexansh <102817421+alexansh@users.noreply.github.com>
2025-10-07 16:04:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.2 MB (37.45%)

---
+++ my-submissions/0033-search-in-rotated-sorted-array/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                ans = i;
+                break;
+            }
+            else{
+                ans = -1;
+            }
+        }
+        return ans;
+    }
+};
