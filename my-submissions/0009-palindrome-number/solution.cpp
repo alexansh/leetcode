eaff5b557767969f291e93917c93f1501c97841f
alexansh <102817421+alexansh@users.noreply.github.com>
2025-11-17 17:05:34 +0000

    Sync LeetCode submission Runtime - 3 ms (43.88%), Memory - 8.6 MB (36.11%)

---
+++ my-submissions/0009-palindrome-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        
+        int temp = x;
+        long long rev = 0;
+        while(x>0){
+            int z = x%10;
+            rev = (rev*10) + z;
+            x = x/10;
+        }
+
+        return rev == temp;
+    }
+};
