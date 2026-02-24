b746a264b8f04cc638abe221e1a642500a87acbf
alexansh <102817421+alexansh@users.noreply.github.com>
2026-02-24 15:46:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.5 MB (35.15%)

--- my-submissions/0316-remove-duplicate-letters/solution.cpp
+++ my-submissions/0316-remove-duplicate-letters/solution.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int> freq(26, 0);
-        vector<bool> inStack(26, false);
+        vector <int> freq(26, 0);
+        vector<bool> used(26,false);
         stack<char> st;
 
-        for (char c : s) freq[c - 'a']++;
+        for(char c:s){
+            freq[c-'a']++;
+        }
+        for(char c : s){
+            freq[c-'a']--;
 
-        for (char c : s) {
-            int idx = c - 'a';
-            freq[idx]--;
+            if(used[c-'a']){
+                continue;
+            }
 
-
-            if (inStack[idx]) continue;
-
-            while (!st.empty() && st.top() > c && freq[st.top() - 'a'] > 0) {
-                inStack[st.top() - 'a'] = false;
+            while(!st.empty() && st.top() > c && freq[st.top() - 'a'] > 0){
+                used[st.top() - 'a'] = false;
                 st.pop();
             }
 
             st.push(c);
-            inStack[idx] = true;
+            used[c - 'a'] = true;
         }
-
-        string ans;
-        while (!st.empty()) {
-            ans.push_back(st.top());
+        string result;
+        while(!st.empty()){
+            result.push_back(st.top());
             st.pop();
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
-
