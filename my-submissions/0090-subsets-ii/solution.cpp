a5c1c91961085424cd2928d709c409db86e94b9e
alexansh <102817421+alexansh@users.noreply.github.com>
2026-03-29 17:14:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.4 MB (84.51%)

---
+++ my-submissions/0090-subsets-ii/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void solve(int index, vector<int>& nums, vector<int>& curr, vector<vector<int>>& ans) {
+        ans.push_back(curr);
+
+        for (int i = index; i < nums.size(); i++) {
+
+            // skip duplicates
+            if (i > index && nums[i] == nums[i - 1]) continue;
+
+            curr.push_back(nums[i]);
+
+            solve(i + 1, nums, curr, ans);
+
+            curr.pop_back(); // backtrack
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+        vector<int> curr;
+
+        solve(0, nums, curr, ans);
+
+        return ans;
+    }
+};
