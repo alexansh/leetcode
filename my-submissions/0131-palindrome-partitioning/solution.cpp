b50798b46f70103b99f6f35829b6891238bb7748
alexansh <102817421+alexansh@users.noreply.github.com>
2026-04-17 14:32:51 +0000

    Sync LeetCode submission Runtime - 34 ms (73.53%), Memory - 53.6 MB (68.16%)

---
+++ my-submissions/0131-palindrome-partitioning/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> result;
+        vector<string> path;
+        backtrack(0, s, path, result);
+        return result;
+    }
+    
+    void backtrack(int start, string &s, vector<string> &path, vector<vector<string>> &result) {
+        if (start == s.size()) {
+            result.push_back(path);
+            return;
+        }
+        
+        for (int end = start; end < s.size(); end++) {
+            if (isPalindrome(s, start, end)) {
+                path.push_back(s.substr(start, end - start + 1));
+                backtrack(end + 1, s, path, result);
+                path.pop_back();
+            }
+        }
+    }
+    
+    bool isPalindrome(string &s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) return false;
+        }
+        return true;
+    }
+};
