9b75debcffb7ac987a171b53cb47a2bc8feb96f8
alexansh <102817421+alexansh@users.noreply.github.com>
2026-04-17 14:34:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 25.2 MB (60.24%)

---
+++ my-submissions/0645-set-mismatch/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int dup = -1, missing = -1;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int idx = abs(nums[i]) - 1;
+            
+            if (nums[idx] < 0) dup = abs(nums[i]);
+            else nums[idx] *= -1;
+        }
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) missing = i + 1;
+        }
+        
+        return {dup, missing};
+    }
+};
