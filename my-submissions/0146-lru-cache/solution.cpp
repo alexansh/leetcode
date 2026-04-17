6c32cab4fdc837e68a50810b84be0bceb4320cf7
alexansh <102817421+alexansh@users.noreply.github.com>
2026-04-17 14:36:50 +0000

    Sync LeetCode submission Runtime - 93 ms (22.01%), Memory - 182.1 MB (17.10%)

---
+++ my-submissions/0146-lru-cache/solution.cpp
@@ -0,0 +1,39 @@
+class LRUCache {
+private:
+    int capacity;
+    
+    list<pair<int,int>> dll; // {key, value}
+    unordered_map<int, list<pair<int,int>>::iterator> mp;
+
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        if (!mp.count(key)) return -1;
+        
+        auto it = mp[key];
+        int value = it->second;
+        
+        dll.erase(it);
+        dll.push_front({key, value});
+        mp[key] = dll.begin();
+        
+        return value;
+    }
+    
+    void put(int key, int value) {
+        if (mp.count(key)) {
+            dll.erase(mp[key]);
+        }
+        else if (dll.size() == capacity) {
+            auto last = dll.back();
+            mp.erase(last.first);
+            dll.pop_back();
+        }
+        
+        dll.push_front({key, value});
+        mp[key] = dll.begin();
+    }
+};
