75aef7fb3edfa685f035fd4c20db85a08db0efe9
alexansh <102817421+alexansh@users.noreply.github.com>
2026-05-26 16:38:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.8 MB (8.59%)

---
+++ my-submissions/3408-count-the-number-of-special-characters-i/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_set<char> lower, upper;
+
+        for (char c : word) {
+            if (islower(c))
+                lower.insert(c);
+            else
+                upper.insert(c);
+        }
+
+        int count = 0;
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (lower.count(c) && upper.count(toupper(c))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
