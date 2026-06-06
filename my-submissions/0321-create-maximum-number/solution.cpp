8c5d545e9794190d9390a051fa9b1b04f349b2f9
alexansh <102817421+alexansh@users.noreply.github.com>
2026-06-06 12:35:13 +0000

    Sync LeetCode submission Runtime - 27 ms (44.88%), Memory - 32.4 MB (37.04%)

---
+++ my-submissions/0321-create-maximum-number/solution.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        vector<int> st;
+        int drop = nums.size() - k;
+
+        for (int num : nums) {
+            while (!st.empty() && drop > 0 && st.back() < num) {
+                st.pop_back();
+                drop--;
+            }
+
+            st.push_back(num);
+        }
+
+        st.resize(k);
+        return st;
+    }
+
+    bool greaterVec(vector<int>& a, int i, vector<int>& b, int j) {
+        while (i < a.size() && j < b.size() && a[i] == b[j]) {
+            i++;
+            j++;
+        }
+
+        if (j == b.size()) return true;
+        if (i == a.size()) return false;
+
+        return a[i] > b[j];
+    }
+
+    vector<int> merge(vector<int>& a, vector<int>& b) {
+        vector<int> ans;
+        int i = 0, j = 0;
+
+        while (i < a.size() || j < b.size()) {
+            if (greaterVec(a, i, b, j))
+                ans.push_back(a[i++]);
+            else
+                ans.push_back(b[j++]);
+        }
+
+        return ans;
+    }
+
+    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
+
+        vector<int> best;
+
+        int start = max(0, k - (int)nums2.size());
+        int end = min(k, (int)nums1.size());
+
+        for (int i = start; i <= end; i++) {
+
+            vector<int> part1 = maxSubsequence(nums1, i);
+            vector<int> part2 = maxSubsequence(nums2, k - i);
+
+            vector<int> candidate = merge(part1, part2);
+
+            if (best.empty() || candidate > best)
+                best = candidate;
+        }
+
+        return best;
+    }
+};
