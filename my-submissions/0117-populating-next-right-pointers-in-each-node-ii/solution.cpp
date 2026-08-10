7c946a195dc5b1587f5e4c1e7f3631aca09bbafe
alexansh <102817421+alexansh@users.noreply.github.com>
2026-08-10 15:27:50 +0000

    Sync LeetCode submission Runtime - 11 ms (45.09%), Memory - 18.9 MB (31.05%)

---
+++ my-submissions/0117-populating-next-right-pointers-in-each-node-ii/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+
+        if (root == NULL)
+            return NULL;
+
+        queue<Node*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+
+            int size = q.size();
+            Node* prev = NULL;
+
+            for (int i = 0; i < size; i++) {
+
+                Node* curr = q.front();
+                q.pop();
+
+                if (prev != NULL)
+                    prev->next = curr;
+
+                prev = curr;
+
+                if (curr->left)
+                    q.push(curr->left);
+
+                if (curr->right)
+                    q.push(curr->right);
+            }
+
+            // Last node of this level
+            prev->next = NULL;
+        }
+
+        return root;
+    }
+};
